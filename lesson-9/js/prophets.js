ababa2d4b274063e98f0c9eab23e6750ab90b507
QueenPendragon <lishelle@a2od.com>
2021-11-11 20:15:13 -0700

    Lesson 9

---
+++ lesson-9/js/prophets.js
@@ -0,0 +1,49 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+ 
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+   // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+   for (let i = 0; i < prophets.length; i++ ) {
+       let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+
+        card.appendChild(h2);
+
+        document.querySelector('div.cards').appendChild(card);
+
+
+    let bday = document.createElement('p');
+
+        bday.textContent = "Date of Birth: " + prophets[i].birthdate;
+
+        card.appendChild(bday);
+
+        document.querySelector('div.cards').appendChild(card);
+
+        let place = document.createElement('p');
+
+        place.textContent = "Place of Birth: " + prophets[i].birthplace;
+
+        card.appendChild(place);
+
+        document.querySelector('div.cards').appendChild(card);
+
+        let image = document.createElement('img');
+
+         image.setAttribute('src', prophets[i].imageurl);
+
+        card.appendChild(image);
+
+        document.querySelector('div.cards').appendChild(card);
+
+       
+   }
+  });
+
