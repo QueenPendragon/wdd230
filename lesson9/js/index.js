f5189ddbabf268e09da9c09f9989038d610875c3
QueenPendragon <lishelle@a2od.com>
2021-11-14 00:28:50 -0700

    index

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -19,4 +19,76 @@
             element.classList.add("hideme");
       }
 
+      //cards JS
+
+      const requestURL = 'json/index.json';
+ 
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+   // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+   for (let i = 0; i < towns.length; i++ ) {
+       let card = document.createElement('section');
+
+    let h2 = document.createElement('h2');
+
+        h2.textContent = towns[i].name;
+
+        card.appendChild(h2);
+
+        document.querySelector('div.cards').appendChild(card);
+
+
+    let motto = document.createElement('p');
+
+        motto.textContent = towns[i].motto;
+
+        card.appendChild(motto);
+
+        document.querySelector('div.cards').appendChild(card);
+
+        let year = document.createElement('p');
+
+        year.textContent = "Founded: " + towns[i].yearFounded;
+
+        card.appendChild(year);
+
+        document.querySelector('div.cards').appendChild(card);
+
+        
+        let pop = document.createElement('p');
+
+        pop.textContent = "Population: " + towns[i].currentPopulation;
+
+        card.appendChild(pop);
+
+        document.querySelector('div.cards').appendChild(card);
+        
+        let rain = document.createElement('p');
+
+        rain.textContent = "Average Rainfall: " + towns[i].averageRainfall;
+
+        card.appendChild(rain);
+
+        document.querySelector('div.cards').appendChild(card);
+
+
+
+        let image = document.createElement('img');
+
+         image.setAttribute('src', towns[i].photo);
+
+        card.appendChild(image);
+
+        document.querySelector('div.cards').appendChild(card);
+
+       
+   }
+  });
+
+
       
