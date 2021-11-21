133178cf42a2f3de6e4843d3d646dffcdf538cff
QueenPendragon <lishelle@a2od.com>
2021-11-20 22:33:28 -0700

    Week 10

---
+++ lesson-10 - Copy/js/index.js
@@ -0,0 +1,94 @@
+//toLocaleDateString
+      const currentDate = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'}
+      document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-us', currentDate);
+
+      function toggleMenu() {
+            document.getElementById("primaryNav").classList.toggle("hide")
+      }
+
+      const today = new Date();
+      console.log(today);
+      
+      const dayNumber = today.getDay();
+      console.log(dayNumber);
+
+      const element = document.querySelector("#message");
+      if (dayNumber == 5) {
+            element.classList.add("showme");
+      } else {
+            element.classList.add("hideme");
+      }
+
+      //cards JS
+
+      const requestURL = 'json/index.json';
+ 
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+   // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+   for (let i = 0; i < towns.length; i++ ) {
+       let card = document.createElement('section');
+
+    let h2 = document.createElement('h2');
+
+        h2.textContent = towns[i].name;
+
+        card.appendChild(h2);
+
+        document.querySelector('div.cards').appendChild(card);
+
+
+    let motto = document.createElement('p');
+
+        motto.textContent = towns[i].motto;
+
+        card.appendChild(motto);
+
+        document.querySelector('div.cards').appendChild(card);
+
+        let year = document.createElement('p');
+
+        year.textContent = "Founded: " + towns[i].yearFounded;
+
+        card.appendChild(year);
+
+        document.querySelector('div.cards').appendChild(card);
+
+        
+        let pop = document.createElement('p');
+
+        pop.textContent = "Population: " + towns[i].currentPopulation;
+
+        card.appendChild(pop);
+
+        document.querySelector('div.cards').appendChild(card);
+        
+        let rain = document.createElement('p');
+
+        rain.textContent = "Average Rainfall: " + towns[i].averageRainfall;
+
+        card.appendChild(rain);
+
+        document.querySelector('div.cards').appendChild(card);
+
+
+
+        let image = document.createElement('img');
+
+         image.setAttribute('src', towns[i].photo);
+
+        card.appendChild(image);
+
+        document.querySelector('div.cards').appendChild(card);
+
+       
+   }
+  });
+
+
+      
---
+++ lesson-10 - Copy/js/preston.js
@@ -0,0 +1,81 @@
+//ADD the key and change units to imperial
+const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5604473&appid=1158060aa6483d99cf7e9eeba387030d&units=imperial";
+
+//Go fetch it and then wait for a response.
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    //Once it comes back, display it to the console.
+    console.log(weatherInfo);
+    
+    
+    document.getElementById("place").innerHTML = weatherInfo.name;
+    document.getElementById("currentCondition").innerHTML = weatherInfo.weather[0].main;
+    document.getElementById("currentTemp").innerHTML = weatherInfo.main.temp;
+    document.getElementById("windChill").innerHTML = weatherInfo.wind.gust;
+    document.getElementById("currentHumidity").innerHTML = weatherInfo.main.humidity;
+    document.getElementById("windSpeed").innerHTML = weatherInfo.wind.speed;
+
+   /* const iconcode = weatherInfo.weather[0].icon;
+    console.log(iconcode);
+    const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+    console.log(icon_path);
+
+    document.getElementById("weather_icon").src = icon_path;*/
+
+ }); //end of "then" fat arrow function
+
+ //Five Day forcast
+const fiveDayapiURL = "//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=1158060aa6483d99cf7e9eeba387030d&units=imperial"
+
+fetch(fiveDayapiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+     //Once it comes back, display it to the console.
+    console.log(weatherInfo);
+
+    console.log(weatherInfo.city.name);
+
+const mydate = new Date();
+
+const y = mydate.getDay();
+console.log(y);
+
+const myweekday =new Array(7);
+myweekday[0] = "Sunday"
+myweekday[1] = "Monday"
+myweekday[2] = "Tuesday"
+myweekday[3] = "Wednesday"
+myweekday[4] = "Thursday"
+myweekday[5] = "Friday"
+myweekday[6] = "Saturday"
+
+console.log(myweekday[3]);
+
+
+
+ const d = new Date();
+
+ const todayDayNumber = d.getDay();
+
+ let forecastDayNumber = todayDayNumber;
+
+ console.log(forecastDayNumber);
+
+ for (i=0; i<weatherInfo.list.length;i++) {
+
+  var time = weatherInfo.list[i].dt_txt;
+  if (time.includes("18:00:00")) {
+    let theDayName = document.createElement("span");
+    theDayName.textContent = myweekday[forecastDayNumber];
+    console.log(">"+myweekday[forecastDayNumber]);
+  }
+
+  forecastDayNumber += 1;
+
+  if (forecastDayNumber===7){
+    forecastDayNumber = 0;
+  }
+}
+ }); //end of "then" fat arrow function
+ 
